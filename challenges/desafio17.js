cc333fc66f8ddd9646cc34faa25bfb46d9e42143
Lucas Monteiro <lucasribeirom@hotmail.com>
2022-07-08 08:33:19 -0300

    Feat: desafio 17

---
+++ challenges/desafio17.js
@@ -0,0 +1,11 @@
+db.resumoProdutos.insertOne(
+  {
+    franquia: "McDonalds",
+    totalProdutos: db.produtos.countDocuments({}),
+  },
+);
+
+db.resumoProdutos.find(
+  {},
+  { _id: false, franquia: true, totalProdutos: true },
+);
