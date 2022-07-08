8cb3514e539d0cb6aeca54687490d99e409cdef9
Lucas Monteiro <lucasribeirom@hotmail.com>
2022-07-08 09:31:33 -0300

    Feat: desafio 22

---
+++ challenges/desafio22.js
@@ -0,0 +1,16 @@
+db.produtos.updateMany(
+  {},
+  { $set: { vendasPorDia: [0, 0, 0, 0, 0, 0, 0] } },
+);
+
+db.produtos.update(
+  { nome: "Big Mac" },
+  { $inc: { "vendasPorDia.3": 60 } },
+);
+
+db.produtos.updateMany(
+  { tags: { $elemMatch: { $eq: "bovino" } } },
+  { $inc: { "vendasPorDia.6": 120 } },
+);
+
+db.produtos.find({}, { _id: false, nome: true, vendasPorDia: true });
